d0f43ee69b7d469337292036676c7c2b24987c6f
Dennis Trautwein <dennis.trautwein@posteo.de>
2020-05-12 20:04:54 +0200

    Bump version to v0.1.2

--- Formula/originstamp-cli.rb
+++ Formula/originstamp-cli.rb
@@ -1,11 +1,8 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class OriginstampCli < Formula
   desc "Go client for the OriginStamp API"
   homepage "https://github.com/dennis-tra/originstamp-cli"
-  url "https://github.com/dennis-tra/originstamp-cli/archive/v0.1.1.tar.gz"
-  sha256 "50bf3785c5cd592f6935d73ee889025513b8bc7a6430b2a39fccbbdefa3c57ec"
+  url "https://github.com/dennis-tra/originstamp-cli/archive/v0.1.2.tar.gz"
+  sha256 "da52306f7b3e1a393c64ccf8aec901d106485fbdcd5b7ccb6b4af6a6411c3402"
 
   depends_on "go" => :build
 
